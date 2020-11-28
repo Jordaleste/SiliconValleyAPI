7c16a86be492eed5f1d59eb9a6b56c5870d0a2ce
Charlie Eison <charlie@esnenterprises.com>
2020-11-28 16:43:44 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+
+const express = require('express');
+
+const app = express();
+
+const fs = require('fs');
+
+const quotes = require('./quotes.json')
+
+app.get('/', function(req, res) {
+    quoteToReturn = Math.floor(Math.random() * quotes.length)
+    res.send(quotes[quoteToReturn])
+});
+
+
+
+
+if (port == null || port == "") {
+    port = 3000;
+  }
+  app.listen(port, function() {
+    console.log('Server started on port 3000')
+});
