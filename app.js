14c1ec3bbf62708b40bf787dd14d1d8d50d6475a
Charlie Eison <charlie@esnenterprises.com>
2020-11-28 16:56:56 -0500

    Made my code way shorter than turners

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-// https://siliconevalleyapi.herokuapp.com/ 
+// https://siliconevalleyapi.herokuapp.com/
 
-const express = require('express');
-
+const express = require("express");
 const app = express();
+const quotes = require("./quotes.json");
 
-const fs = require('fs');
-
-const quotes = require('./quotes.json')
-
-app.get('/', function(req, res) {
-    quoteToReturn = Math.floor(Math.random() * quotes.length)
-    res.send(quotes[quoteToReturn])
+app.get("/", function (req, res) {
+    quoteToReturn = Math.floor(Math.random() * quotes.length);
+    res.send(quotes[quoteToReturn]);
 });
 
-
-let port = process.env.PORT
+let port = process.env.PORT;
 
 if (port == null || port == "") {
     port = 3000;
-  }
-  app.listen(port, function() {
-    console.log('Server started on port 3000')
+}
+app.listen(port, function () {
+    console.log("Server started on port 3000");
 });
